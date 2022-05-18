f3236f55ff6f0366f7659666897731044f824191
liuweiguo <1159428886@qq.com>
2022-05-18 09:12:31 +0800

    first-of-array

--- easy-quesions/05firstOfArray/template.ts
+++ easy-quesions/05firstOfArray/template.ts
@@ -1,1 +1,18 @@
-type First<T extends any[]> = T extends {length:0}?never:T[0]
+//第一种
+// type First<T extends any[]> = T extends []?never:T[0]
+
+//第二种 indexed
+// type First<T extends any[]> = T["length"] extends 0 ? never : T[0]
+
+//第三种
+type ages = [1,2,3]
+
+//union
+type t1 = ages[number]
+
+//看看某个值是不是在union里面
+//第三种
+// type First<T extends any[]> = T[0] extends T[number] ? T[0] : never
+
+//第四种 infer类型推断
+type First<T extends any[]> = T extends [infer First,... infer rest] ? First : never
